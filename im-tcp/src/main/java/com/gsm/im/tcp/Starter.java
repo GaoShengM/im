1fe1c9abb30b290a9eef5cc8c0c4e62e1bd9f2b9
Gaoshengmin <gsm0614@outlook.com>
2023-07-08 23:25:33 +0800

    编写TCP的协议模块

--- im-tcp/src/main/java/com/gsm/im/tcp/Starter.java
+++ im-tcp/src/main/java/com/gsm/im/tcp/Starter.java
@@ -2,6 +2,10 @@ package com.gsm.im.tcp;
 
 import com.gsm.im.tcp.server.LimServer;
 import com.gsm.im.tcp.server.LimWebSocketServer;
+import io.netty.bootstrap.BootstrapConfig;
+import org.yaml.snakeyaml.Yaml;
+
+import java.io.*;
 
 /**
  * @author MSI-PC
@@ -10,8 +14,11 @@ import com.gsm.im.tcp.server.LimWebSocketServer;
  * @date 2023/7/8 18:01
  */
 public class Starter {
-    public static void main(String[] args) {
-         new LimServer(9000);
-         new LimWebSocketServer(19000);
+    public static void main(String[] args) throws FileNotFoundException {
+        Yaml yaml = new Yaml();
+        InputStream inputStream = new FileInputStream("I:\\TempCodeSpace\\im\\im-tcp\\src\\main\\resources\\config.yml");
+        BootstrapConfig bootstrapConfig = yaml.loadAs(inputStream, BootstrapConfig.class);
+        new LimServer(9000);
+        new LimWebSocketServer(19000);
     }
 }
